3898e7ae526c7bc7d1ccc51c54b9bda7ead00418
jacobalvarez248 <jacobalvarez248@gmail.com>
2025-08-28 10:11:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ if 'selected_team' not in st.session_state:
 query_params = st.query_params
 selected_team = query_params.get("selected_team", "")
 default_tab = "📊 Team Dashboards" if selected_team else "🏆 Rankings"
-tab_choice = st.radio("View Mode", ["🏆 Rankings", "📊 Team Dashboards"], horizontal=True, index=0 if default_tab == "🏆 Rankings" else 1)
+tab_choice = st.radio(" ", ["🏆 Rankings", "📊 Team Dashboards"], horizontal=True, label_visibility="hidden", index=0 if default_tab == "🏆 Rankings" else 1)
 
 #-----------------------------------------------------RANKINGS TAB------------------------------------------------
 if tab_choice == "🏆 Rankings":
